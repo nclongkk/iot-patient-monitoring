5efc4c5be0029aa30e8083a04e37629a6e75fbd9
nclongkk <nclongkk@gmail.com>
2023-12-03 11:23:04 +0700

    Handle unhandle exception

--- src/main.ts
+++ src/main.ts
@@ -23,3 +23,9 @@ async function bootstrap() {
   });
 }
 bootstrap();
+process.on('uncaughtException', (err) => {
+  console.error('uncaughtException', err);
+});
+process.on('unhandledRejection', (err) => {
+  console.error('unhandledRejection', err);
+});
